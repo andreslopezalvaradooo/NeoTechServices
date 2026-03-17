b20abd72ef9c9f7a085c6cfacdf22432318d041d
DESKTOP-DHEBCDJ\andre <andreslopez>
2026-03-17 14:29:26 -0500

    se cambia tipo de strokeWidth

--- client/components/ui/spinner.tsx
+++ client/components/ui/spinner.tsx
@@ -1,11 +1,24 @@
-import { HugeiconsIcon } from "@hugeicons/react"
-import { Loading03Icon } from "@hugeicons/core-free-icons"
-import { cn } from "@/src/lib/utils"
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Loading03Icon } from "@hugeicons/core-free-icons";
+import { cn } from "@/src/lib/utils";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  strokeWidth,
+  ...props
+}: Omit<React.ComponentProps<"svg">, "strokeWidth"> & {
+  strokeWidth?: number;
+}) {
   return (
-    <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    <HugeiconsIcon
+      icon={Loading03Icon}
+      strokeWidth={strokeWidth ?? 2}
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  );
 }
 
-export { Spinner }
+export { Spinner };
