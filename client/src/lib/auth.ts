936d6eea3b2ffc056199fb8aa64a139ae9e70770
DESKTOP-DHEBCDJ\andre <andreslopez>
2026-04-16 12:27:51 -0500

    trustedOrigins

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -5,4 +5,9 @@ import prisma from "./prisma";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: { enabled: true },
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://192.168.80.40:3000",
+    "https://tudominio.com",
+  ],
 });
